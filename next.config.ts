242042d10b35f1bfb3e812851c9fdc02d8b6504c
Andrii Stadnyk <stadnyk.andy@gmail.com>
2025-03-01 17:35:10 +0200

    add new image host

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'utfs.io',
       },
+      {
+        protocol: 'https',
+        hostname: 'zi7nqxyoik.ufs.sh',
+      },
     ],
   },};
 
