4fdb64476fde1a668456d7e3b451507ca9037cec
Andrii Stadnyk <stadnyk.andy@gmail.com>
2025-06-15 19:49:48 +0300

    video gallery slider

---
+++ src/components/Home/Video/Slider.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { VideoCard } from "./VideoCard";
+import { YoutubeVideo } from "@prisma/client";
+import { MotionDiv, Title } from "common";
+import { useEffect, useRef, useState } from "react";
+import { MdArrowForwardIos } from "react-icons/md";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper as SwiperT } from "swiper/types";
+
+import { cn } from "utils/cn";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+
+type SliderProps = {
+  data: YoutubeVideo[];
+};
+
+export const Slider = ({ data }: SliderProps) => {
+  const [showNav, setShowNav] = useState(false);
+  const swiperRef = useRef<SwiperT>(null);
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      const visibleSlides =
+        typeof swiperRef.current.params.slidesPerView === "number"
+          ? swiperRef.current.params.slidesPerView
+          : swiperRef.current.slidesPerViewDynamic();
+
+      setShowNav(visibleSlides >= data.length);
+    }
+  }, [swiperRef, data]);
+
+  return (
+    <>
+      <MotionDiv
+        initial={{ opacity: 0, x: -20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        viewport={{ once: true }}
+        className={"mb-8 flex items-end justify-between"}
+      >
+        <Title size={"6xl"}>Video</Title>
+        <div className={cn("flex gap-x-2", { hidden: showNav })}>
+          <button
+            onClick={() => swiperRef.current?.slidePrev()}
+            className={
+              "group flex items-center justify-center rounded-full bg-white/70 p-2 shadow-[-2px_2px_5px_rgb(69,41,37,.05)] backdrop-blur md:p-3"
+            }
+          >
+            <MdArrowForwardIos
+              className={
+                "size-5 rotate-180 text-ok_main-600 transition-colors group-hover:text-ok_orange-400 md:size-6"
+              }
+            />
+          </button>
+          <button
+            onClick={() => swiperRef.current?.slideNext()}
+            className={
+              "group flex items-center justify-center rounded-full bg-white/70 p-2 shadow-[2px_2px_5px_rgb(69,41,37,.05)] backdrop-blur md:p-3"
+            }
+          >
+            <MdArrowForwardIos
+              className={
+                "size-5 text-ok_main-600 transition-colors group-hover:text-ok_orange-400 md:size-6"
+              }
+            />
+          </button>
+        </div>
+      </MotionDiv>
+      <MotionDiv
+        initial={{ opacity: 0, x: -20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 0.4 }}
+        viewport={{ once: true }}
+      >
+        <Swiper
+          loop
+          speed={900}
+          draggable
+          spaceBetween={12}
+          slidesPerView={1}
+          breakpoints={{
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+        >
+          {data.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className={"aspect-video overflow-hidden rounded-2xl"}>
+                <VideoCard url={item.url} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </MotionDiv>
+    </>
+  );
+};
--- src/components/Home/Video/VideoGallery.tsx
+++ src/components/Home/Video/VideoGallery.tsx
@@ -1,66 +1,16 @@
-"use client";
+import { Slider } from "./Slider";
+import { Container } from "common";
 
-import { VideoCard } from "./VideoCard";
-import { Container, MotionDiv, Title } from "common";
+import { prismaDB } from "lib/db";
 
-const urls = [
-  {
-    id: "regfdr4835y2turwgsfdh",
-    url: "https://youtu.be/tet2yOt8ewk?si=E_yW4g3bNmNodo8g",
-  },
-  {
-    id: "sdthrgsbfdjvhfkwrgesbd",
-    url: "https://youtu.be/Xv1HsejFcV8?si=a6axCqWbmFIojor9",
-  },
-];
+export async function VideoGallery() {
+  const videoUrls = await prismaDB.youtubeVideo.findMany();
 
-export function VideoGallery() {
   return (
     <section>
       <Container>
         <div className={"py-8"}>
-          <MotionDiv
-            initial={{ opacity: 0, x: -20 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.8, delay: 0.2 }}
-            viewport={{ once: true }}
-          >
-            <Title size={"6xl"} className={"mb-8"}>
-              Video
-            </Title>
-          </MotionDiv>
-          <MotionDiv
-            initial={"hidden"}
-            whileInView={"visible"}
-            transition={{
-              staggerChildren: 0.3,
-            }}
-            viewport={{ once: true }}
-            className={"grid grid-cols-1 gap-4 md:grid-cols-2"}
-          >
-            {urls.map((item) => (
-              <MotionDiv
-                variants={{
-                  hidden: {
-                    opacity: 0,
-                    y: 20,
-                  },
-                  visible: {
-                    opacity: 1,
-                    y: 0,
-                  },
-                }}
-                transition={{
-                  duration: 0.8,
-                  ease: "easeInOut",
-                }}
-                key={item.id}
-                className={"aspect-video overflow-hidden rounded-2xl"}
-              >
-                <VideoCard url={item.url} />
-              </MotionDiv>
-            ))}
-          </MotionDiv>
+          <Slider data={videoUrls} />
         </div>
       </Container>
     </section>
